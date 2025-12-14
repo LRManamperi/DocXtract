e666d8a336e7a95dcefad02f441201690ecab097
Rasa <134576794+LRManamperi@users.noreply.github.com>
2025-12-14 10:54:38 +0530

    update

---
+++ UI/page_modules/__init__.py
@@ -0,0 +1,16 @@
+"""Page modules for DocXtract Dashboard"""
+
+from .home import render_home
+from .charts_analysis import render_charts_analysis
+from .chart_data_tables import render_chart_data_tables
+from .testing import render_testing
+from .about import render_about
+
+__all__ = [
+    'render_home',
+    'render_charts_analysis',
+    'render_chart_data_tables',
+    'render_testing',
+    'render_about'
+]
+
